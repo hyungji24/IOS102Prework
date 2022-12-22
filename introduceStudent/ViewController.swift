1572f1c4e1fd264023f25140afb6e1af6ce13c9c
hyungji24 <hyungjikim@email.arizona.edu>
2022-12-22 16:52:11 +0900

    Initial commit

---
+++ introduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introduceStudent
+//
+//  Created by hyungji24 on 12/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
