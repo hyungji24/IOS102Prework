2b7a19ea12305675246ecca9e5c4e21e52314b6c
hyungji24 <hyungjikim@email.arizona.edu>
2022-12-22 18:02:28 +0900

    Step 3: Adding Logic to the App

--- introduceStudent/ViewController.swift
+++ introduceStudent/ViewController.swift
@@ -23,6 +23,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var morePetsSwitch: UISwitch!
     
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+        
+        
+        print(introduction)
+    }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
