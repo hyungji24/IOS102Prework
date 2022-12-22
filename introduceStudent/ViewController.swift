25aecb6416ce41c6d63df976434478315c3731aa
hyungji24 <hyungjikim@email.arizona.edu>
2022-12-22 17:18:03 +0900

    Step 2: Connecting Storyboard Elements to ViewController

--- introduceStudent/ViewController.swift
+++ introduceStudent/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
